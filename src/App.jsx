7480f09c7bd4238e37d348c09be1c4074ae8243d
gzcmustafa <gozcumustafa34@gmail.com>
2024-11-13 14:52:38 +0300

    feat: add TransactionList and TransactionListS components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import "./App.css";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList  from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   const title = "Basix Expense Tracker";
 
   return (
-    <div>
+    <GlobalProvider>
       <Header title={title} />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList/>
+        <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
