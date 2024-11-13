55ee21fb6bea6f93d0999249cc79f0d504f23f84
gzcmustafa <gozcumustafa34@gmail.com>
2024-11-13 17:01:12 +0300

    feat: add Transaction delete btn function

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -17,10 +17,20 @@ export const GlobalContext = createContext(initialState);
 //Provider Component
 export const GlobalProvider = ({children}) => {
     const [state,dispatch] = useReducer(AppReducer,initialState);
+
+//Actions
+function deleteTransaction(id) {
+    dispatch({
+        type:'DELETE_TRANSACTION',
+        payload:id
+    }
+    )
+}
+    
     
     return (
             
-            <GlobalContext.Provider value={{transactions:state.transactions}}>
+            <GlobalContext.Provider value={{transactions:state.transactions , deleteTransaction}}>
                 {children}
             </GlobalContext.Provider>
 
